0fd62c9c47871c2bc107eb526bf07fe6f6e44087
happtur <andrea.eriksson@helsinki.fi>
2018-02-11 21:01:08 +0200

    teht. 3

--- index.js
+++ index.js
@@ -33,6 +33,17 @@ app.get('/info', (req, res) => {
   res.send(`<p>puhelinluettelossa ${persons.length} henkilön tiedot</p><p>${new Date()}</p>`)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if(person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
