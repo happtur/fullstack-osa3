d0f559e4fad65440862482447f11602fc33b2528
happtur <andrea.eriksson@helsinki.fi>
2018-02-12 23:12:25 +0200

    teht. 9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-var morgan = require('morgan')
+const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
 
@@ -87,7 +89,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
